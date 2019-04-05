567158c12d6eef1cdbbf4e5436c871ee9eae0a49
TurianKoYoT <dimadegalevich@yandex.ru>
2019-04-05 18:17:10 +0500

    Get n max numbers from input stream
    - do not load whole file in memory
    - n is the only parameter
    - spec
    - as gem

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -1,6 +1,16 @@
 require "nmax/version"
+require_relative "binary_heap"
 
 module Nmax
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.find_max_numbers(stream, count)
+    max_numbers = BinaryHeap.new
+
+    stream.each(sep = ' ') do |number|
+      max_numbers << number.to_i
+      max_numbers.pop if max_numbers.length > count
+    end
+
+    max_numbers.numbers.shift
+    puts max_numbers.numbers
+  end
 end
